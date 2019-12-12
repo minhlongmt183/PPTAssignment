7ab2ec955df048f70c6dc499906234e4368edb5c
MinhLong <long.voitpro2k@hcmut.edu.vn>
2019-12-12 00:40:07 -0500

    sua lan 4

---
+++ QuickTest/Bai1/Spline3RangBuoc.py
@@ -0,0 +1,125 @@
+import numpy as np
+#########################################################
+# INPUT:                                                #
+# x và y là các vector chứa các giá trị đối số và       #
+#  của hàm.                                             #
+# OUTPUT:                                               #
+#   a,b,c,d là các vector của hệ số spline              #
+#########################################################
+
+def tridiag(N, A, B, C, D):
+    u = np.zeros(N)
+    l = np.zeros(N)
+    v = np.zeros(N)
+    y = np.zeros(N)
+    X = np.zeros(N)
+
+    u[0] = A[0]
+    v[0] = B[0]
+    l[0] = C[0]/u[0]
+    y[0] = D[0]
+
+    for i in range(1, N-1):
+        u[i] = A[i] - l[i-1]*v[i-1]
+        v[i] = B[i]
+        l[i] = C[i] / u[i]
+        y[i] = D[i] -l[i-1]*y[i-1]
+
+    u[N-1] = A[N-1] - l[N-2]*v[N-2]
+    y[N-1] = D[N-1] - l[N-2]*y[N-2]
+    X[N-1] = y[N-1]/u[N-1]
+    for i in range(N-2,-1,-1):
+        X[i] = (y[i]-v[i]*X[i+1]) / u[i]
+    return X
+
+
+
+def cspline(x, y, alpha, beta):
+    N = min(np.size(x), np.size(y))
+    h = np.zeros(N-1)
+
+    for i in range(N-1):
+        h[i] = x[i+1] - x[i]
+
+    A = np.zeros(N)
+    B = np.zeros(N-1)
+    C = np.zeros(N-1)
+    D = np.zeros(N)
+
+    print(len(h), N)
+    A[0] = 2*h[0]
+    #A[N-1] = 2*h[N-2]
+    B[0] = h[0]
+    C[0] = h[0]
+    
+
+    for i in range(1,N-1):
+        A[i] = 2*(h[i-1] + h[i])
+        B[i] = h[i]
+        C[i] = h[i]
+
+    D[0] = 3*(y[1] - y[0]) / h[0] - 3*alpha
+    #D[N-1] = 3*beta -3*(y[N-1] - y[N-2])/h[N-2]
+    
+    for i in range(1,N-1):
+        D[i] = 3*(y[i+1] - y[i])/h[i] - 3*(y[i] - y[i-1])/h[i-1]
+
+    
+    c = tridiag(N,A,B,C,D)
+    
+    print("ma tran c = {}".format(c))
+
+
+    a = np.zeros(N-1)
+    b = np.zeros(N-1)
+    d = np.zeros(N-1)
+    for i in range(N-1):
+        a[i] = y[i]
+        b[i] = (y[i+1]-y[i])/h[i] - h[i]*(c[i+1]+2*c[i])/3
+        d[i] = (c[i+1]-c[i])/3/h[i]
+
+    return [a,b,c,d]
+
+
+def InputMatrix(M, N, Matrix):
+    M = int(input("Nhap cap ma tran M:"))
+    N = int(input("Nhap cap ma tran N:"))
+    #input matrx from user:
+    print("nhap he so ma tran [ " + str(M) + " x " + str(N)+" ]")
+
+    Matrix = []
+    for i in range(M):
+        a = []
+        for j in range(N):
+            print("input element [ " + str(i) + "," + str(j)+" ]")
+            a.append(int(input()))
+        Matrix.append(a)
+    return (M,N,Matrix)
+    
+
+if __name__ == "__main__":
+    # print("nhap ma tran A")
+    # Ma = 0
+    # Na = 0
+    # A = []
+    # (Ma, Na, A) = InputMatrix(Ma, Na, A)
+    # print()
+    # print("nhap ma tran B")
+    # Mb = 0
+    # Nb = 0
+    # B = []
+    # (Mb, Nb, B) = InputMatrix(Mb, Nb, B)
+
+    # alpha = float(input("nhap alpha: "))
+    # beta = float(input("nhap beta: "))
+    X = np.array([0.5,0.6,0.9, 1.1, 1.2, 1.3, 1.4])
+    Y = np.array([2.65, 2.82, 3.46, 4.0, 4.32, 4.67,5.05])
+    alpha = 4
+    beta = 0
+
+    (a,b,c,d) = cspline(X,Y,alpha,beta)
+
+    print("ham so")
+    for i in range(np.size(a)):
+        print("{0:.2f} + {1:.2f}(x-{4:.2f}) + {2:.2f}(x-{4:.2f})^2 + {3:.2f}(x-{4:.2f})^3".format(a[i], b[i], c[i], d[i], X[i]))
+
